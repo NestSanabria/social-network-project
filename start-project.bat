d3df190ea4f11386ee6fb1211cc1353b821e0359
NestSanabria <nestsanabria@outlook.com>
2024-09-03 23:56:43 -0500

    Update

--- start-project.bat
+++ start-project.bat
@@ -1,2 +1,2 @@
-start cmd.exe /k "cd /d C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:\TalentoTech\bdata mongodb"
+:: start cmd.exe /k "cd /d C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:\TalentoTech\bdata mongodb"
 start cmd.exe /k "cd /d C:\TalentoTech\api-social-network && npm run start:watch"
